3774a4001c18d8f8fdea3afa5f947666a9fc2511
Oliver61178 <Oliver61178@GB-LD1LPW10G039.mastek.com>
2019-07-12 14:22:41 +0100

    spring bean app

---
+++ hrapp/src/main/java/com/mastek/training/hrapp/apis/EmployeeService.java
@@ -0,0 +1,28 @@
+package com.mastek.training.hrapp.apis;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+import com.mastek.training.hrapp.entities.Employee;
+
+//Component: indicate to spring to create an object of this class
+//scope: singleton: one object for application [default]
+// 		 prototype: one object for each usage
+
+@Component
+@Scope("singleton")
+public class EmployeeService {
+
+
+	public EmployeeService() {
+		System.out.println("Employee Service Created");
+	}
+	
+	
+	
+	public Employee registerEmployee(Employee emp) {
+		System.out.println("Employee Registered"+emp);
+		return emp;
+	}
+
+}
--- hrapp/src/test/java/com/mastek/training/hrapp/HrappApplicationTests.java
+++ hrapp/src/test/java/com/mastek/training/hrapp/HrappApplicationTests.java
@@ -2,15 +2,46 @@ package com.mastek.training.hrapp;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import com.mastek.training.hrapp.apis.EmployeeService;
+import com.mastek.training.hrapp.entities.Employee;
+
+//Initialize the junit test with spring boot application env
+//spring boot test: used to test spring application
+//with all the test cases identified
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class HrappApplicationTests {
 
+	
+	//scan in memory all the components and provide 
+	//the best match object in the component
+	@Autowired
+	EmployeeService empService;
+	//object creation should be handled by string 
+	@Autowired
+	Employee emp;
+	
+	@Test
+	public void addEmployeeUsingService() {
+	/*	Employee emp = new Employee();
+		emp.setEmpno(112);
+		emp.setName("Example");
+		emp.setSalary(333);
+		
+		above is not needed
+	*/
+		empService.registerEmployee(emp);
+	
+	}
+	
 	@Test
 	public void contextLoads() {
+		System.out.println("System Test Executed");
 	}
 
 }
